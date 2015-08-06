465ffaf2e0e25a9babb3fc7802d470544f7a0d82
Luwei <bug@csie.io>
2015-08-06 12:53:37 +0800

    Refactor to split def

--- spelling_check.py
+++ spelling_check.py
@@ -1,4 +1,4 @@
-import re
+import re, os
 import json
 import time
 import enchant
@@ -7,7 +7,8 @@ from enchant.tokenize import EmailFilter, URLFilter
 from fluentmail import *
 
 # Global Settings #
-jsWordPath 		= 'D:\\js.json'
+pathList = ['D:\\word1.json', 
+	'D:\\word2.json']
 
 debugMode = False
 
@@ -26,106 +27,117 @@ def remove_variable(text):
 	result = re.sub("(\$t\(.*?\))|(__.*?__)|(^--.*?--$)", " ", text)
 	return result
 
-# d = enchant.Dict("en_US")
+def remove_han(text):
+	result = text
+	for n in re.findall(ur'[\u4e00-\u9fff]+',text):
+		result = re.sub(n, " ", result)
+	return result
+
+def spellingCheck(jsonPath):
+	inputTypo = {}
+
+	input_file  = file(jsonPath, "r")
+	inputWord = json.loads(input_file.read().decode("utf-8"))
+
+	for key in inputWord :
+		text = inputWord[key]
+		# print 'TEXT: ' + text
+		filterText = remove_variable(remove_HTML_tags(remove_HTML_encode(remove_han(text))))
+		# print 'FILTERED: ' + filterText
+		chkr.set_text(filterText)
+		
+		errList = []
+
+		if debugMode:
+			for err in chkr:
+				# print (err.word)
+				errList.append(err.word)
+
+			if len(errList) > 0 :
+				print 'TEXT: ' + text
+
+				err_string = ''
+				for item in errList:
+					err_string += item + ' '
+				print 'Typo: ' + err_string
+				print '--------------------------------------------------'
+		else:
+			for err in chkr:
+				errList.append(err.word)
+
+			if len(errList) > 0 :
+				inputTypo[text] = {}
+				inputTypo[text]['typo'] = errList
+				inputTypo[text]['key']  = key
+	return inputTypo
+
+def createTypoTable(inputTypo, filename):
+	typoTable = ''
+	if len(inputTypo) > 0 :
+		typoTable = '\
+			<h1>'+filename+' Typo<h1>\
+			<table class="tg">\
+				<tr>\
+					<th class="tg">Key</th>\
+					<th class="tg">String</th>\
+				</tr>';
+
+		for text in inputTypo:
+			resultText = text
+			for typo in inputTypo[text]['typo']:
+				resultText = resultText.replace(typo, '<font color="red">'+typo+'</font>')
+			
+			typoTable += '\
+			<tr class="tg">\
+				<td class="tg">'+inputTypo[text]['key']+'</td>\
+				<td class="tg">'+resultText+'</td>\
+			</tr>'
+
+		typoTable += '</table>'
+
+	return typoTable
+
+def sendMail(mailHTMLContent):
+	mail = FluentMail('smtp.mail.com', 587, TLS)
+
+	mail.credentials('key', 'secret')\
+		.from_address('ME <ME@mail.com.tw>')\
+		.to('YO <yo@mail.com.tw>')\
+		.subject('Spelling Check Result')\
+		.body(mailHTMLContent)\
+		.as_html()\
+		.send()
+
+def runSpellingCheck(pathList):
+	mailHTMLContent = ''
+	mailHeader 		= ''
+	mailTable 		= ''
+
+	checkTime = time.strftime("%Y/%m/%d")
+
+	mailHeader = '<h1>Spelling Check Result '+checkTime+'</h1>\
+		<style type="text/css">\
+			.tg  {border-collapse:collapse;border-spacing:0;}\
+			.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}\
+			.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}\
+		</style>'
+
+	for path in pathList:
+		filename = os.path.basename(path)
+
+		inputTypo = spellingCheck(path)
+
+		mailHeader += '<h3> '+filename+' Typo: '+ str(len(inputTypo)) + '</h3>'
+		mailTable  += createTypoTable(inputTypo, filename)
+
+	mailHTMLContent = mailHeader + mailTable
+
+	if(mailTable != ''):
+		sendMail(mailHTMLContent)
+
+'''
+	MAIN
+'''
 chkr = SpellChecker("en_US",filters=[EmailFilter,URLFilter])
 
-jsWord = {}
-jsTypo = {}
-
-'''
-	js.json
-'''
-if debugMode:
-	print '\n------------- mup -------------\n'
-input_mup_file  = file(jsWordPath, "r")
-jsWord = json.loads(input_mup_file.read().decode("utf-8"))
-
-for key in jsWord :
-	text = jsWord[key]
-	text = text.encode('ascii','ignore')
-	# print 'TEXT: ' + text
-	filterText = remove_variable(remove_HTML_tags(remove_HTML_encode(text)))
-	# print 'FILTERED: ' + filterText
-	chkr.set_text(filterText)
-	
-	errList = []
-
-	if debugMode:
-		for err in chkr:
-			# print (err.word)
-			errList.append(err.word)
-
-		if len(errList) > 0 :
-			print 'TEXT: ' + text
-
-			err_string = ''
-			for item in errList:
-				err_string += item + ' '
-			print 'Typo: ' + err_string
-			print '--------------------------------------------------'
-	else:
-		for err in chkr:
-			errList.append(err.word)
-
-		if len(errList) > 0 :
-			jsTypo[text] = {}
-			jsTypo[text]['typo'] = errList
-			jsTypo[text]['key']  = key
-
-'''
-	Compose mail
-'''
-
-mailHTMLContent = ''
-mailHeader 		= '';
-typoTable 	= '';
-mailTranslationTable = '';
-
-checkTime = time.strftime("%Y/%m/%d");
-
-mailHeader = '\
-	<h1>Spelling Check Result '+checkTime+'</h1>\
-	<h3> Typo: '+ str(len(jsTypo)) + '</h3>';
-
-''' typoTable '''
-typoTable = '\
-	<h1>MUP Typo<h1>\
-	<style type="text/css">\
-		.tg  {border-collapse:collapse;border-spacing:0;}\
-		.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}\
-		.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}\
-	</style>\
-	<table class="tg">\
-		<tr>\
-			<th class="tg">Key</th>\
-			<th class="tg">String</th>\
-		</tr>';
-
-for text in jsTypo :
-	resultText = text
-	for typo in jsTypo[text]['typo']:
-		resultText = resultText.replace(typo, '<font color="red">'+typo+'</font>')
-	
-	typoTable += '\
-	<tr class="tg">\
-		<td class="tg">'+jsTypo[text]['key']+'</td>\
-		<td class="tg">'+resultText+'</td>\
-	</tr>';
-
-typoTable += '</table>';
-
-'''
-	Send mail
-'''
-
-mailHTMLContent = mailHeader + typoTable
-
-mail = FluentMail('smtp.server.com', 587, TLS)
-
-mail.credentials('name', 'pass')\
-	.from_address('name <your@mail.com>')\
-	.to('rv <rcv@mail.com>')\
-	.subject('Spelling Check Result')\
-	.body(mailHTMLContent)\
-	.as_html()\
-	.send()
+runSpellingCheck(pathList)
